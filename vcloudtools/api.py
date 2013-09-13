1e927436282fec8863f67f9a4bd1f7f355bbec36
Grant Cohoe <grant@grantcohoe.com>
2013-09-13 14:15:52 -0400

    added OrgNets, ExtNets, OrgVcds, ExtNetList, and Iprange objects to module

--- vcloudtools/api.py
+++ vcloudtools/api.py
@@ -7,7 +7,7 @@ import logging
 import lxml.etree
 import requests
 
-from vcloudtools.vcloud import Link, OrgList, Org
+from vcloudtools.vcloud import *
 
 log = logging.getLogger(__name__)
 
@@ -18,6 +18,8 @@ VCLOUD_MIME = 'application/*+xml;version=%s' % VCLOUD_VERSION
 VCLOUD_NS = {
     'vcloud': 'http://www.vmware.com/vcloud/v%s' % VCLOUD_VERSION
 }
+
+OVF_NS = { 'ovf': 'htp://schemas.dmtf.org/ovf' }
 
 
 class ClientError(Exception):
@@ -49,6 +51,9 @@ class VCloudAPIClient(object):
 
         if self.logged_in:
             self._links = self._fetch_initial_links()
+
+        self.VCLOUD_NS = VCLOUD_NS
+        self.OVF_NS = OVF_NS
 
         log.debug("Created %s", self)
 
@@ -100,6 +105,8 @@ class VCloudAPIClient(object):
         )
 
         self.token = res.headers[VCLOUD_AUTH_HEADER]
+        # At __init__ self.logged_in is not set and thus never populates the list
+        self._links = self._fetch_initial_links()
 
     def browse(self, path='/'):
         """
@@ -107,6 +114,15 @@ class VCloudAPIClient(object):
         """
         res = self._req('get', self._url(path))
         return res
+
+    def ext_net_list(self):
+        """
+        Retrieve a list of all external networks
+        """
+        res = self._req('get', self._lookup('admin.vcloud'))
+
+        etree = lxml.etree.fromstring(res.content)
+        return _parse_ext_net_list(etree)
 
     def org_list(self):
         """
@@ -127,6 +143,60 @@ class VCloudAPIClient(object):
 
         etree = lxml.etree.fromstring(res.content)
         return _parse_org(etree)
+
+    def org_nets(self, org):
+        """
+        Retrieve the networks for a given organization
+        """
+        nets = []
+        for l in org.links['application/vnd.vmware.vcloud.orgNetwork+xml']:
+            res = self._req('get', l.href)
+            etree = lxml.etree.fromstring(res.content)
+
+            nets.append(_parse_org_nets(etree))
+
+        return nets
+
+    def org_vdcs(self, org):
+        """
+        Retrieve a list of organization virtualDataCenters
+        """
+        vdcs = []
+        for l in org.links['application/vnd.vmware.vcloud.vdc+xml']:
+            res = self._req('get', l.href)
+            etree = lxml.etree.fromstring(res.content)
+            vdcs.append(_parse_org_vdcs(etree))
+
+        return vdcs
+
+    def org_vapps(self, org):
+        """
+        Retrieve a list of vapps for a given organization
+        """
+        ovdcs = self.org_vdcs(org)
+        r_entities = []
+        for ovdc in ovdcs:
+            r_entities += ovdc.entities['application/vnd.vmware.vcloud.vApp+xml']
+        return r_entities
+
+
+    def ext_net(self, name):
+        """
+        Retrieve an external network by name
+        """
+        ext_net_short = self.ext_net_list().ext_net_by_name(name)
+
+        res = self._req('get', ext_net_short.href)
+
+        etree = lxml.etree.fromstring(res.content)
+        return _parse_ext_net(etree)
+
+    def ip_ranges(self, el):
+        config = el.find('vcloud:Configuration', VCLOUD_NS)
+        scope = config.find('vcloud:IpScope', VCLOUD_NS)
+        iprange = scope.find('vcloud:IpRanges', VCLOUD_NS)
+        ranges = [r for r in iprange.findall('vcloud:IpRange', VCLOUD_NS)]
+        return ranges
 
     @property
     def token(self):
@@ -160,6 +230,70 @@ def envget(key, default=None):
         default
     )
 
+def _parse_org_vdcs(el):
+    type_ = el.attrib['type']
+    href  = el.attrib['href']
+    name  = el.attrib['name']
+    id_   = el.attrib['id']
+
+    raw_storage_capacity = el.find('vcloud:StorageCapacity', VCLOUD_NS)
+    s_cap = {}
+    for k in raw_storage_capacity:
+        s_cap[k.tag.split('}')[1]] = k.text
+
+    raw_compute_capacity = el.find('vcloud:ComputeCapacity', VCLOUD_NS)
+    c_cap = {'cpu':{}, 'memory':{}}
+    raw_cpu = raw_compute_capacity.find('vcloud:Cpu', VCLOUD_NS)
+    for k in raw_cpu:
+        c_cap['cpu'][k.tag.split('}')[1]] = k.text
+    raw_memory = raw_compute_capacity.find('vcloud:Memory', VCLOUD_NS)
+    for k in raw_memory:
+        c_cap['memory'][k.tag.split('}')[1]] = k.text
+
+    links = _parse_links(el)
+    r_entities = _parse_resource_entities(el)
+
+    # TODO: AvailableNetworks, Capabilities, Quotas, etc
+
+    return OrgVdc(
+        type=type_,
+        href=href,
+        name=name,
+        id=id_,
+        storage=s_cap,
+        compute=c_cap,
+        links=links,
+        entities=r_entities
+    )
+
+
+def _parse_org_nets(el):
+    configuration = el.find('vcloud:Configuration', VCLOUD_NS)
+    ipscope = configuration.find('vcloud:IpScope', VCLOUD_NS)
+    
+    gateway = ipscope.find('vcloud:Gateway', VCLOUD_NS).text
+    netmask = ipscope.find('vcloud:Netmask', VCLOUD_NS).text
+    dns = ipscope.find('vcloud:DnsSuffix', VCLOUD_NS).text
+    ranges = _parse_ip_ranges(el.attrib['name'], ipscope.find('vcloud:IpRanges', VCLOUD_NS))
+
+    return OrgNet(
+        el.attrib['name'],
+        gateway,
+        netmask,
+        dns,
+        ranges
+    )
+
+def _parse_ip_ranges(net_name, el):
+    raw_ranges = el.findall('vcloud:IpRange', VCLOUD_NS)
+
+    ranges = []
+    for r in raw_ranges:
+        first = r.find('vcloud:StartAddress', VCLOUD_NS).text
+        last = r.find('vcloud:EndAddress', VCLOUD_NS).text
+        name = net_name+" - ("+first+"-"+last+")"
+        ranges.append(IpRange(first,last))
+    return ranges
 
 def _parse_links(el):
     res = defaultdict(list)
@@ -168,19 +302,54 @@ def _parse_links(el):
         res[link.type].append(link)
     return res
 
+def _parse_resource_entities(el):
+    el = el.find('vcloud:ResourceEntities', VCLOUD_NS)
+    res = defaultdict(list)
+    for c in el.findall('vcloud:ResourceEntity', VCLOUD_NS):
+        re = _parse_resource_entity(c)
+        res[re.type].append(re)
+    return res
 
 def _parse_link(el):
     return Link(**el.attrib)
 
+def _parse_resource_entity(el):
+    return ResourceEntity(**el.attrib)
+
+def _parse_ext_net_list(el):
+    ext_nets = [_parse_ext_net_short(c) for c in el.find('vcloud:Networks', VCLOUD_NS).findall('vcloud:Network', VCLOUD_NS)]
+    return ExtNetList(ext_nets=ext_nets)
 
 def _parse_org_list(el):
     orgs = [_parse_org_short(c) for c in el.findall('vcloud:Org', VCLOUD_NS)]
     return OrgList(orgs=orgs)
 
+def _parse_ext_net_short(el):
+    return ExtNet(**el.attrib)
 
 def _parse_org_short(el):
     return Org(**el.attrib)
 
+def _parse_ext_net(el):
+    type_ = el.attrib['type']
+    href  = el.attrib['href']
+    name  = el.attrib['name']
+    id_   = el.attrib['id']
+
+    description = el.find('vcloud:Description', VCLOUD_NS).text
+    config = el.find('vcloud:Configuration', VCLOUD_NS)
+
+    links = _parse_links(el)
+
+    return ExtNet(
+        type=type_,
+        href=href,
+        name=name,
+        config=config,
+        id=id_,
+        description=description,
+        links=links
+    )
 
 def _parse_org(el):
     type_ = el.attrib['type']
@@ -209,5 +378,4 @@ def _custom_raise_for_status(res):
         res.raise_for_status()
     except requests.RequestException as err:
         raise APIError(err)
-
 
--- vcloudtools/vcloud.py
+++ vcloudtools/vcloud.py
@@ -5,11 +5,20 @@ class Link(_Link):
     def __new__(cls, type, href, rel, name=None):
         return super(cls, Link).__new__(cls, type, href, rel, name)
 
+_ResourceEntity = namedtuple('ResourceEntity', 'type href name')
+class ResourceEntity(_ResourceEntity):
+    def __new__(cls, type, href, name=None):
+        return super(cls, ResourceEntity).__new__(cls, type, href, name)
+
 _Org = namedtuple('Org', 'type href name id full_name description links')
 class Org(_Org):
     def __new__(cls, type, href, name, id=None, full_name=None, description=None, links=None):
         return super(cls, Org).__new__(cls, type, href, name, id, full_name, description, links)
 
+_OrgVdc = namedtuple('OrgVdc', 'type href name id storage compute links entities')
+class OrgVdc(_OrgVdc):
+    def __new__(cls, type, href, name, id=None, storage=None, compute=None, links=None, entities=None):
+        return super(cls, OrgVdc).__new__(cls, type, href, name, id, storage, compute, links, entities)
 
 _OrgList = namedtuple('OrgList', 'orgs')
 class OrgList(_OrgList):
@@ -19,3 +28,27 @@ class OrgList(_OrgList):
             if o.name == name:
                 return o
         return None
+
+_ExtNetList = namedtuple('ExtNetList', 'ext_nets')
+class ExtNetList(_ExtNetList):
+
+    def ext_net_by_name(self, name):
+        for en in self.ext_nets:
+            if en.name == name:
+                return en
+        return None
+
+_ExtNet = namedtuple('ExtNet', 'type href name config id description links')
+class ExtNet(_ExtNet):
+    def __new__(cls, type, href, name, config=None, id=None, description=None, links=None):
+        return super(cls, ExtNet).__new__(cls, type, href, name, config, id, description, links)
+
+_OrgNet = namedtuple('OrgNet', 'name gateway netmask dns ranges')
+class OrgNet(_OrgNet):
+    def __new__(cls, name, gateway, netmask, dns, ranges):
+        return super(cls, OrgNet).__new__(cls, name, gateway, netmask, dns, ranges)
+
+_IpRange = namedtuple('IpRange', 'first last')
+class IpRange(_IpRange):
+    def __new__(cls, first, last):
+        return super(cls, IpRange).__new__(cls, first, last)
